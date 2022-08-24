f449ffa49780604928e9110f8a0603363bf144a6
YLisDev <evgeniy7lisit@gmail.com>
2022-08-24 23:41:23 +0300

    Confirmation feature added

---
+++ components/SavingsPlanner/features/ConfirmButton.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import { Button } from '../UI';
+
+const ConfirmButton = () => {
+    const [buttonText, setButtonText] = useState('Confirm');
+
+    const handleClick = () => {
+        // set new text and undo after timeout
+        setButtonText('You are awesome!');
+        setTimeout(() => {
+            setButtonText('Confirm');
+        }, 1500);
+    };
+
+    return <Button onClick={handleClick}>{buttonText}</Button>;
+};
+
+export default ConfirmButton;
