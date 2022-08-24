2025688b53d351014c547e01bb55393a6e6dcf28
YLisDev <evgeniy7lisit@gmail.com>
2022-08-25 00:14:47 +0300

    Next.js svg setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+    trailingSlash: true,
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack', 'url-loader'],
+        });
+        return config;
+    },
+};
